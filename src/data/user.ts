4d29180e98b0a6cc17eb5e068ac4badc4cc5bb7c
crfogato <crfogato@gmail.com>
2026-01-13 11:50:30 -0300

    feat: implement home page layout with header and sidebar components

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,8 +3,9 @@ import { User } from "@/types/user";
 export const user: User = {
   slug: "crfogato",
   name: "Rodrigo Fogato",
-  avatar: "",
-  cover: "",
+  avatar: "https://ui-avatars.com/api/?name=R+F",
+  cover:
+    "https://br.freepik.com/fotos-gratis/oculos-deitado-no-laptop-refletindo-a-luz-da-tela-no-escuro_172419727.htm#fromView=keyword&page=1&position=1&uuid=3ef94360-308f-4067-b52e-8c5771ef032f&query=Capa+linkedin+programador",
   bio: "Desenvolvedor",
   postCount: 145,
 };
