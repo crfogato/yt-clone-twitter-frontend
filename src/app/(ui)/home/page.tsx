1a146cd40e61c19a23af31e6b8b61db362c7f337
crfogato <crfogato@gmail.com>
2026-01-13 14:56:27 -0300

    feat: add tweet components and home feed functionality

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,9 +1,13 @@
 import { HomeHeader } from "@/components/home/home-header";
+import { TweetPost } from "@/components/tweet/tweet-post";
+import { HomeFeed } from "./home-feed";
 
 export default function Page() {
   return (
     <div className="">
       <HomeHeader />
+      <TweetPost />
+      <HomeFeed />
     </div>
   );
 }
